21e05769de5c0e2b4f6f27433f676b3171303c05
vargo <jvargo@gmail.com>
2017-12-08 22:17:41 -0800

    fix paths add tables

--- app.R
+++ app.R
@@ -5,13 +5,17 @@ library(tidyverse)
 devtools::install_github("ITHIM/ITHIM", ref="devel")
 library("ITHIM")
 
-PAfileName <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/activeTransportTime.csv"
-BURfileName <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/burden.portland.csv"
-POPfileName <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/F.portland.csv"
+# PAfilePath <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/activeTransportTime.csv"
+# BURfilePath <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/burden.portland.csv"
+# POPfilePath <- "https://raw.githubusercontent.com/ITHIM/ITHIM/devel/inst/F.portland.csv"
 
-PAexample <- system.file("activeTravelOHAS.csv", package = "ITHIM")
-BURexample <- system.file("gbd_Manuscript_2011-2015.csv", package = "ITHIM")
-POPexample <- system.file("F.portland.11_21_2017.csv", package = "ITHIM")
+PAexamplePath <- system.file("activeTravelOHAS.csv", package = "ITHIM")
+BURexamplePath <- system.file("gbd_Manuscript_2011-2015.csv", package = "ITHIM")
+POPexamplePath <- system.file("F.portland.11_21_2017.csv", package = "ITHIM")
+
+PAdownload <- read.csv(PAexamplePath, header=T)
+BURdownload <- read.csv(BURexamplePath, header=T)
+POPdownload <- read.csv(POPexamplePath, header=T)
 
 
 ui <- shinyUI(pageWithSidebar(
@@ -49,9 +53,9 @@ mainPanel(
   # Output: Tabset w/ plot, summary, and table ----
   tabsetPanel(type = "tabs",
               tabPanel("Introduction", textOutput("This is a web interface for the Intergrated Transport Health Impact Model (ITHIM),")),
-              tabPanel("Input - Physical Activity", plotOutput('PA')),
-              tabPanel("Input - Disease Burden", plotOutput('burden')),
-              tabPanel("Input - Population",   plotOutput('pop')), 
+              tabPanel("Input - Physical Activity", plotOutput('PAplot'), dataTableOutput('PAtab')),
+              tabPanel("Input - Disease Burden", plotOutput('BURplot'), dataTableOutput('BURtab')),
+              tabPanel("Input - Population",   plotOutput('POPplot'), dataTableOutput('POPtab')), 
               tabPanel("Baseline - Summary", verbatimTextOutput('summary'))
   )
   
@@ -61,9 +65,10 @@ mainPanel(
 
 server <- function(input, output, session) {
   
+  
   ##### PHYSICAL ACTIVITY #######
   
-  output$PA <- renderPlot({
+  output$PAplot <- renderPlot({
   # input$file1 will be NULL initially. After the user selects
   # and uploads a file, it will be a data frame with 'name',
   # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -73,9 +78,7 @@ server <- function(input, output, session) {
   inFilePA <- input$file1
   
   
-  PAfile <- read.csv(ifelse(is.null(inFilePA),system.file("activeTravelOHAS.csv", package = "ITHIM"),
-                            inFilePA$datapath),
-                            header = T, sep=",")
+  PAfile <- read.csv(ifelse(is.null(inFilePA),PAexamplePath,inFilePA$datapath),header = T, sep=",")
   
     ggplot(PAfile, aes(x=ageClass, y=value, fill=sex)) + geom_bar(stat="identity", position="dodge") + 
     facet_grid(mode ~ .) +
@@ -84,18 +87,30 @@ server <- function(input, output, session) {
   })
   
   
+  output$PAtab <- renderDataTable({
+    
+    
+    inFilePA <- input$file3
+    
+    PAfile <- read.csv(ifelse(is.null(inFilePA),PAexamplePath,inFilePA$datapath), header = T, sep=",") 
+    
+    PAfile
+    
+    
+  })
+  
   # Downloadable csv of selected dataset ----
   output$downloadPAexample <- downloadHandler(
     filename = "ActiveTransportTime.csv",
     content = function(file) {
-      write.csv(PAexample, file, row.names = FALSE)
+      write.csv(PAdownload, file, row.names = FALSE)
     }
   )
   
   ##### BURDEN ###########
   
   
-  output$burden <- renderPlot({
+  output$BURplot <- renderPlot({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -105,8 +120,7 @@ server <- function(input, output, session) {
     inFileBurden <- input$file2
   
     
-    burdenFile <- read.csv(ifelse(is.null(inFileBurden),system.file("gbd_Manuscript_2011-2015.csv", package = "ITHIM"),
-                                  inFileBurden$datapath), header = T, sep=",") 
+    burdenFile <- read.csv(ifelse(is.null(inFileBurden),BURexamplePath,inFileBurden$datapath), header = T, sep=",") 
     
       ggplot(burdenFile, aes(x=ageClass, y=value, fill=sex)) + geom_bar(stat="identity", position="dodge") + 
       facet_grid(burdenType ~ disease) +
@@ -114,11 +128,23 @@ server <- function(input, output, session) {
   })
   
   
+  output$BURtab <- renderDataTable({
+    
+    
+    inFileBUR <- input$file3
+    
+    BURfile <- read.csv(ifelse(is.null(inFileBUR),BURexamplePath,inFileBUR$datapath), header = T, sep=",") 
+    
+    BURfile
+    
+    
+  })
+  
   # Downloadable csv of selected dataset ----
   output$downloadBURexample <- downloadHandler(
     filename = "PortlandBurden.csv",
     content = function(file) {
-      write.csv(BURexample, file, row.names = FALSE)
+      write.csv(BURdownload, file, row.names = FALSE)
     }
   )
   
@@ -127,7 +153,7 @@ server <- function(input, output, session) {
   ##### POPULATION ###########
   
   
-  output$pop <- renderPlot({
+  output$POPplot <- renderPlot({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
@@ -136,11 +162,21 @@ server <- function(input, output, session) {
     
    inFilePOP <- input$file3
     
-   POPfile <- read.csv(ifelse(is.null(inFilePOP),system.file("F.portland.11_21_2017.csv", package = "ITHIM"),
-                              inFilePOP$datapath), header = T, sep=",") 
+   POPfile <- read.csv(ifelse(is.null(inFilePOP),POPexamplePath,inFilePOP$datapath), header = T, sep=",") 
    
       ggplot(POPfile, aes(x=ageClass, y=value, fill=sex)) + geom_bar(stat="identity", position="dodge") + 
       ggtitle("Age-Sex Population Distribution") 
+  })
+  
+  output$POPtab <- renderDataTable({
+    
+    inFilePOP <- input$file3
+    
+    POPfile <- read.csv(ifelse(is.null(inFilePOP),POPexamplePath,inFilePOP$datapath), header = T, sep=",") 
+    
+    POPfile
+  
+    
   })
   
   
@@ -148,7 +184,7 @@ server <- function(input, output, session) {
   output$downloadPOPexample <- downloadHandler(
     filename = "PortlandPopulation.csv",
     content = function(file) {
-      write.csv(POPexample, file, row.names = FALSE)
+      write.csv(POPdownload, file, row.names = FALSE)
     }
   )
   
@@ -158,13 +194,17 @@ server <- function(input, output, session) {
   
   output$summary <- renderText({
     
-    inFilePA <- ifelse(is.null(input$file1), PAexample, input$file1)
-    inFileBUR <- ifelse(is.null(input$file2), BURexample, input$file2)
-    inFilePOP <- ifelse(is.null(input$file3), POPexample, input$file3)
+    inFilePA <- ifelse(is.null(input$file1), PAexamplePath, input$file1$datapath)
+    inFileBUR <- ifelse(is.null(input$file2), BURexamplePath, input$file2$datapath)
+    inFilePOP <- ifelse(is.null(input$file3), POPexamplePath, input$file3$datapath)
     
     
-    ITHIM.baseline <- createITHIM(activeTransportFile = PAfileName, GBDFile = BURfileName, FFile = POPfileName)
+    ITHIM.baseline <- createITHIM(activeTransportFile = inFilePA, GBDFile = inFileBUR, FFile = inFilePOP)
     ITHIM.2027.constrained <- update(ITHIM.baseline, list(muwt = 44.91, muct = 17.82))
+    
+    ITHIM.baseline
+    
+    ITHIM.2027.constrained
     
     deltaBurden(ITHIM.baseline, ITHIM.2027.constrained, bur = "daly", dis = "CVD")
     
