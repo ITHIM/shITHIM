ccd9456c4978f5697a244d82030af7f47b1510fa
vargo <jvargo@gmail.com>
2017-12-18 22:12:55 -0800

    add reactive functions

--- app.R
+++ app.R
@@ -63,7 +63,12 @@ mainPanel(
               tabPanel("Input - Physical Activity", plotOutput('PAplot'), dataTableOutput('PAtab')),
               tabPanel("Input - Disease Burden", plotOutput('BURplot'), dataTableOutput('BURtab')),
               tabPanel("Input - Population",   plotOutput('POPplot'), dataTableOutput('POPtab')), 
-              tabPanel("Output - Summary", verbatimTextOutput('summary'))
+              tabPanel("Output - Summary", 
+                       textOutput('summary'), 
+                       hr(), 
+                       textOutput('baseline.sum'), 
+                       hr(),
+                       textOutput('scenario.sum'))
   )
   
   )
@@ -91,11 +96,11 @@ server <- function(input, output, session) {
     facet_grid(mode ~ .) +
     ggtitle("Age-Sex Active Transport Times") +
     ylab("Minutes per Week")
-  })
+
+})
   
   
   output$PAtab <- renderDataTable({
-    
     
     inFilePA <- input$file3
     
@@ -126,7 +131,6 @@ server <- function(input, output, session) {
     
     inFileBurden <- input$file2
   
-    
     burdenFile <- read.csv(ifelse(is.null(inFileBurden),BURexamplePath,inFileBurden$datapath), header = T, sep=",") 
     
       ggplot(burdenFile, aes(x=ageClass, y=value, fill=sex)) + geom_bar(stat="identity", position="dodge") + 
@@ -137,13 +141,11 @@ server <- function(input, output, session) {
   
   output$BURtab <- renderDataTable({
     
-    
     inFileBUR <- input$file3
     
     BURfile <- read.csv(ifelse(is.null(inFileBUR),BURexamplePath,inFileBUR$datapath), header = T, sep=",") 
     
     BURfile
-    
     
   })
   
@@ -195,54 +197,49 @@ server <- function(input, output, session) {
     }
   )
   
-  output$baselineSum <- renderText({
-    
-    holder <- paste0("The estimated number of deaths prevented by current walking and cycling levels is ", lives0,
-                     ". Under the suggested scenario, mean walking time increased by ",input$newWalk,
-                     "% (to ", round(newWalk,0)," min/week) and mean cycling time increased by ",input$newCycle,
-                     "% (to ", round(newCycle,0)," min/week). This would prevent an additional ",lives.scenario," lives per year.")
-    
-    holder
-    
-    
-  })
   
   ##### summary in create ITHIM File ###########
   
-  output$summary <- renderText({
+  ITHIM.baseline <- reactive({
     
     inFilePA <- ifelse(is.null(input$file1), PAexamplePath, input$file1$datapath)
     inFileBUR <- ifelse(is.null(input$file2), BURexamplePath, input$file2$datapath)
     inFilePOP <- ifelse(is.null(input$file3), POPexamplePath, input$file3$datapath)
     
     
-    ITHIM.baseline <- createITHIM(activeTransportFile = inFilePA, GBDFile = inFileBUR, FFile = inFilePOP)
-    startWalk <- ITHIM.baseline@parameters@muwt
-    startCycle <- ITHIM.baseline@parameters@muct
-    newWalk <- startWalk*((input$newWalk/100)+1)
-    newCycle <- startCycle*((input$newCycle/100)+1)
-    
-    ITHIM.walk0 <- update(ITHIM.baseline, list(muwt = 0.1,muct = 0.1))
-    
-    lives0 <- round(deltaBurden(ITHIM.baseline, ITHIM.walk0, bur = "deaths" , dis = "all"),0)
-    ITHIM.scenario <- update(ITHIM.baseline, list(muwt = newWalk, 
-                                                  muct = newCycle))
-    lives.scenario <- round(deltaBurden(ITHIM.baseline, ITHIM.scenario, bur = "deaths" , dis = "all"),0)
-    
-    
-    ITHIM.scenario <- update(ITHIM.baseline, list(muwt = newWalk, 
-                                                  muct = newCycle))
-    lives.scenario <- round(deltaBurden(ITHIM.baseline, ITHIM.scenario, bur = "deaths" , dis = "all"),0) *-1
-    
-    holder <- paste0("The estimated number of deaths prevented by current walking and cycling levels is ", lives0,
-           ". Under the suggested scenario, mean walking time increased by ",input$newWalk,
-           "% (to ", round(newWalk,0)," min/week) and mean cycling time increased by ",input$newCycle,
-           "% (to ", round(newCycle,0)," min/week). This would prevent an additional ",lives.scenario," deaths per year.")
-    
-    holder
+    return(createITHIM(activeTransportFile = inFilePA, GBDFile = inFileBUR, FFile = inFilePOP))
     
   })
   
+    
+  
+newWalk <- reactive({ITHIM.baseline()@parameters@muwt*((input$newWalk/100)+1)})
+newCycle <- reactive({ITHIM.baseline()@parameters@muct*((input$newCycle/100)+1)})   
+    
+ITHIM.scenario <- reactive({
+
+  update(ITHIM.baseline(), list(muwt = newWalk(), muct = newCycle()))
+
+    })
+    
+  output$summary <- renderText({
+    
+    lives0 <- round(deltaBurden(ITHIM.baseline(), update(ITHIM.baseline(), list(muwt = 0.1,muct = 0.1)), bur = "deaths" , dis = "all"),0)
+    
+    lives.scenario <- round(deltaBurden(ITHIM.baseline(), ITHIM.scenario(), bur = "deaths" , dis = "all"),0) *-1
+    
+    paste0("The estimated number of deaths prevented by current walking and cycling levels is ", lives0,
+                                       ". Under the suggested scenario, mean walking time increased by ",input$newWalk,
+                                       "% (to ", round(newWalk(),0)," min/week) and mean cycling time increased by ",input$newCycle,
+                                       "% (to ", round(newCycle(),0)," min/week). This would prevent an additional ",lives.scenario," deaths per year.")
+  })
+  
+  # output$baseline.sum <- dataITHIM()$ITHIM.baseline
+  # 
+  # output$scenario.sum <- dataITHIM()$ITHIM.scenario
+  
+  
 }
+
 
 shinyApp(server = server, ui = ui)
